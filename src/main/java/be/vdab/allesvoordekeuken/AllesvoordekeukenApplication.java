3456e709ea7d9816e8c091ac0ecf5b17bf8d3e85
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-05-14 09:19:39 +0200

    eerste commit alles voor de keuken

---
+++ src/main/java/be/vdab/allesvoordekeuken/AllesvoordekeukenApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.allesvoordekeuken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllesvoordekeukenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllesvoordekeukenApplication.class, args);
+	}
+}

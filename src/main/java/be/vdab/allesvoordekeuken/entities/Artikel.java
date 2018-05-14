b55d2b4aaed40f4e4f21f904b6aa11de45cd865c
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-05-14 09:41:01 +0200

    na invoegen entity Artikel

---
+++ src/main/java/be/vdab/allesvoordekeuken/entities/Artikel.java
@@ -0,0 +1,35 @@
+package be.vdab.allesvoordekeuken.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="artikels")
+public class Artikel implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	private long id;
+	private String naam;
+	private BigDecimal aankoopprijs;
+	private BigDecimal verkoopprijs;
+	
+	protected Artikel() {
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public String getNaam() {
+		return naam;
+	}
+	public BigDecimal getAankoopprijs() {
+		return aankoopprijs;
+	}
+	public BigDecimal getVerkoopprijs() {
+		return verkoopprijs;
+	}
+}
